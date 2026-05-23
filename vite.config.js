9281362f36f1626369d17783249752edee05502b
dilaraj <dilara.ajaj369@gmail.com>
2026-05-23 22:22:26 +1000

    Fixing vite.config bug

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {host: true},
-  base: '/Article-Preview-Master-Component'
+  base: '/Article-Preview-Master-Component/'
 })
