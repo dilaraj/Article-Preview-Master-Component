b0b28c3bd015a754a1f6c81323c93f4e6f53c433
dilaraj <dilara.ajaj369@gmail.com>
2026-05-28 19:23:29 +1000

    Creating desktop compatibility

--- src/App.jsx
+++ src/App.jsx
@@ -18,19 +18,23 @@ export default function App() {
       <div className="article-img-container">
         <img src={articleImg} alt="Article Img" className="article-img" />
       </div>
-      <div className="article-info-container">
-        <div className="article-header">
-            Shift the overall look and feel by adding these wonderful touches to furniture in your home
+      <div className="article-info-main-container">
+        <div className="article-info-container">
+          <div className="article-header">
+              Shift the overall look and feel by adding these wonderful touches to furniture in your home
+          </div>
+          <div className="article-preview-content">
+            Ever been in a room and felt like something was missing? Perhaps it felt slighlty bare and uninviting. I've got some simple tips to help you make any room feel complete.
+          </div>
         </div>
-        <div className="article-preview-content">
-          Ever been in a room and felt like something was missing? Perhaps it felt slighlty bare and uninviting. I've got some simple tips to help you make any room feel complete.
-        </div>
-      </div>
-      {isOpened ? 
+        {isOpened ? 
           (<AuthorInfo toggle={toggle}/>)
           :
           (<ShareOption toggle={toggle}/>)
-      }
+        }
+      </div>
+      
+      
     </div>
   )
 }
